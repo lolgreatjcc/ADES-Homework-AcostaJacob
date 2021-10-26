e6300661bc58a116731645108f90e93f12ac4034
lolgreatjcc <acostalolgreatjcc@gmail.com>
2021-10-26 14:22:56 +0800

    Added: Homework 1

---
+++ Homework Week 1/app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const app = express();
+const port = 3000
+
+
+/**
+ *  Homework 1 Submission
+ *  Name: Acosta Jan Jacob Domalanta
+ *  Admin No.: P2026271
+ *  Class: DIT/2B03
+ */
+
+app.use(cors());
+app.use(bodyParser.json())
+
+function logRequests(req,res,next) {
+    // Logs the original path
+    //console.log(req);
+
+    // Logs the request path.
+    //console.log(req.path)
+    
+    next();
+}
+app.use(logRequests);
+
+app.get("/timenow", function (req, res) {
+    const d = new Date();
+    var timenow = `${d.getHours()}h:${d.getMinutes()}m:${d.getSeconds()}s`
+    res.status(200).send(timenow);
+})
+
+app.post("/squareValue", function (req, res) {
+    var value = req.query.value;
+    if (isNaN(value)) {
+        res.status(500).send({ "errorMsg": "Please input a number." })
+    }
+    else {
+        var output = value * value;
+        res.status(200).send({ "output": output + "" });
+    }
+
+})
+
+app.post("/cubeValue", function (req, res) {
+    var value = req.body.value;
+    console.log(req.body);
+    if (isNaN(value)) {
+        res.status(500).send({ "errorMsg": "Please input a number." })
+    }
+    else {
+        var output = value * value * value;
+        res.status(200).send({ "output": output + "" });
+    }
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send({ "errorMsg": "Resouce not found!"});
+})
+
+app.listen(port, () => {
+    console.log(`Homework 1 App listening at http://localhost:${port}`);
+})
