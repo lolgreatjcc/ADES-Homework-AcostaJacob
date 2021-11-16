945d1f94e9146a8a31c040e2555f5231cb3ea91b
lolgreatjcc <acostalolgreatjcc@gmail.com>
2021-11-16 13:23:57 +0800

    Another update

--- Week_3/databaseModel.js
+++ Week_3/databaseModel.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'b7ae6c8a4557d4',
-    password: '817375f',
+    password: '817375f8',
     database: 'heroku_2230e5a57d0e906'
 });
 
